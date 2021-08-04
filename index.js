4dc4d1bad79c99671024382807492a1dba7ee63b
sierraskyewallace <sskwallace@gmail.com>
2021-08-04 10:49:36 -0700

    done

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    let filter = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = [];
+    let arrayOfWords = string.split(" ")
+    for (let i = 0; i < arrayOfWords.length; i++) {
+      if (i == 0) {
+        result.push(this.capitalize(arrayOfWords[i]))
+      } else {
+        if (filter.includes(arrayOfWords[i])) {
+          result.push(arrayOfWords[i])
+        } else {
+          result.push(this.capitalize(arrayOfWords[i]))
+        }
+      }
+    }
+    return result.join(" ");
+  }
 }
